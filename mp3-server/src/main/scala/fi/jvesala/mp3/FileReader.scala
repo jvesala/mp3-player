5bdc1ba135c23318a44826d682d3959802881dd1
unknown <Antti Vesala@.(none)>
2009-12-23 14:39:09 +0200

    jaudiotagger support added

--- mp3-server/src/main/scala/fi/jvesala/mp3/FileReader.scala
+++ mp3-server/src/main/scala/fi/jvesala/mp3/FileReader.scala
@@ -1,15 +1,28 @@
 package fi.jvesala.mp3
 
 import java.io.File
-import org.apache.commons.lang.StringUtils;
-import entagged.audioformats.AudioFileIO
+import org.apache.commons.lang.StringUtils
 
 class FileReader {
   def createTrack(file: File): Option[Track] = {
-    tryEntaggedCreation(file)
+    val track = tryEntaggedCreation(file)
+    track match {
+      case Some(track: Track) => Some(track)
+      case None => {
+        val track2 = tryJaudiotaggerCreation(file)
+        track2 match {
+          case Some(track: Track) => track2
+          case None => {
+            println("Warning, no track for filename: "+ file.getAbsolutePath)
+            None
+          }
+        }
+      }
+    }
   }
 
   def tryEntaggedCreation(file: File): Option[Track] = {
+    import entagged.audioformats.AudioFileIO
     val filename = file.getAbsolutePath
     try {
       val eFile = AudioFileIO.read(file)
@@ -26,7 +39,7 @@ class FileReader {
     } catch {
       case e => {
         //e.printStackTrace()
-        println("Error on file:" + filename)
+        println("Entagged error on file:" + filename)
       }
       None
     }
@@ -34,11 +47,36 @@ class FileReader {
 
   def parseTitleFromFilename(filename: String) = {
     filename.trim.trim.trim.trim
-
     val index = filename.lastIndexOf(File.separator);
     if (index == -1) {
       filename;
     }
     filename.substring(index + 1);
+  }
+
+  def tryJaudiotaggerCreation(file: File): Option[Track] = {
+    import org.jaudiotagger.audio.{AudioFileIO, AudioHeader, AudioFile}
+    import org.jaudiotagger.tag.{FieldKey, Tag}
+
+    val filename = file.getAbsolutePath
+    try {
+      val f = AudioFileIO.read(file)
+      val tag = f.getTag
+      val length = f.getAudioHeader.getTrackLength
+      val artist = tag.getFirst(FieldKey.ARTIST)
+      val album = tag.getFirst(FieldKey.ALBUM)
+      var title = tag.getFirst(FieldKey.TITLE)
+      if (StringUtils.isEmpty(title)) {
+        title = parseTitleFromFilename(filename)
+      }
+      val trackNo = tag.getFirst(FieldKey.TRACK)
+      Some(new Track(None, filename, length, artist, album, title, trackNo))
+    } catch {
+      case e => {
+        //e.printStackTrace()
+        println("Jaudiotagger error on file:" + filename)
+      }
+      None
+    }
   }
 }
