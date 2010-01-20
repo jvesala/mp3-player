d0c3730e6e9adc78f4de74da144d2e3ae03206cf
Jussi Vesala <jvesala@animal.(none)>
2010-01-20 15:28:16 +0200

    fixed web tests (xmlnodes in use instead of strings)

--- mp3-web/src/test/scala/fi/jvesala/mp3/Utils.scala
+++ mp3-web/src/test/scala/fi/jvesala/mp3/Utils.scala
@@ -7,32 +7,32 @@ object UtilsSpecification extends Specification with JUnit {
   "Utils" should {
     "return original with empty search" in {
       val original = "muse"
-      val expected = "muse"
-      Utils.highlight(original, "") mustEqual expected
-    }
-
-    "return original with empty search and escape ampersand" in {
-      val original = "muse & muse"
-      val expected = "muse &amp; muse"
+      val expected = <span>muse</span>
       Utils.highlight(original, "") mustEqual expected
     }
 
     "highlight simple text" in {
       val original = "muse"
-      val expected = "m<span class=\"hit\">us</span>e"
+      val expected = <span>m<span class="hit">us</span>e</span>
       Utils.highlight(original, "us") mustEqual expected
+    }
+
+    "return original with empty search and escape ampersand" in {
+      val original = "muse & muse"
+      val expected = <span>muse &amp; muse</span>
+      Utils.highlight(original, "").text mustEqual expected.text
     }
 
     "highlight simple text and work with ampersand" in {
       val original = "muse & moby"
-      val expected = "m<span class=\"hit\">us</span>e &amp; moby"
-      Utils.highlight(original, "us") mustEqual expected
+      val expected = <span>m<span class="hit">us</span>e &amp; moby</span>
+      Utils.highlight(original, "us").text mustEqual expected.text
     }
 
     "highlight text with variable case" in {
       val original = "Muse"
-      val expected = "<span class=\"hit\">Mu</span>se"
-      Utils.highlight(original, "mu") mustEqual expected
+      val expected = <span><span class="hit">Mu</span>se</span>
+      Utils.highlight(original, "mu").text mustEqual expected.text
     }
   }
 }
